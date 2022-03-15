7dc6ec102a4702f51c46f0d74bd665a4540d5a3b
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2022-03-15 16:36:47 +0000

    ShareButton placed in own component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React from "react";
 import "./App.css";
-import ResponsiveAppBar from "./ResponsiveAppBar";
 import "@fontsource/roboto/400.css";
-import Button from "@mui/material/Button";
-import SaveIcon from "@mui/icons-material/Save";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 import PrimarySearchAppBar from "./PrimarySearchAppBar.js";
 import BasicTable from "./BasicTable";
-import Hero from "./Hero.js";
-import StarUI from "./StarUI.js"
-import ShareIcon from '@mui/icons-material/Share';
+import StarUI from "./StarUI.js";
+import ShareButton from "./ShareButton";
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
       <PrimarySearchAppBar />
-      <StarUI/>
-      <Button
-        endIcon={<ShareIcon/>}
-        sx={{
-          fontSize: "1.5rem",
-          marginBottom: "2rem"
-        }}
-        variant="contained"
-        color="secondary"
-      >
-        Share Star
-      </Button>
-      <BasicTable/>
-  
+      <StarUI />
+      <ShareButton />
+      <BasicTable />
     </div>
   );
 }
---
+++ src/ShareButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import ShareIcon from "@mui/icons-material/Share";
+
+export default function ShareButton() {
+  return (
+    <Button
+      endIcon={<ShareIcon />}
+      sx={{
+        fontSize: "1.5rem",
+        marginBottom: "2rem",
+      }}
+      variant="contained"
+      color="secondary"
+    >
+      Share Star
+    </Button>
+  );
+}
